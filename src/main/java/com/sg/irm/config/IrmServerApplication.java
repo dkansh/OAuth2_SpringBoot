f5ab23f5fcf9f780baf6e54547d00ee4f79690fa
Deepak Kumar <www.dkansh.gov.in@gmail.com>
2018-11-16 17:28:39 +0530

    Initial commit

---
+++ src/main/java/com/sg/irm/config/IrmServerApplication.java
@@ -0,0 +1,18 @@
+package com.sg.irm.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@EntityScan("com.sg.irm.entity")
+@EnableJpaRepositories("com.sg.irm.repository")
+@SpringBootApplication(scanBasePackages={"com.sg"})
+public class IrmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrmServerApplication.class, args);
+	}
+}
